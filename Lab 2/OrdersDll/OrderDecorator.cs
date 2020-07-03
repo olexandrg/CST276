f91ff265a1ba7512b1ea8b15ce44ec246dcb14ec
olexandrg <olexandrg@tuta.io>
2020-07-03 14:44:32 -0700

    Implemented passthrough behavior.

--- Lab 2/OrdersDll/OrderDecorator.cs
+++ Lab 2/OrdersDll/OrderDecorator.cs
@@ -17,29 +17,17 @@ namespace OrdersDll
 
         public override void AddItem(string productCode, int quantity, double cost, double weight)
         {
-            items.Add(new OrderItem(productCode, quantity, cost, weight));
+           order.AddItem(productCode, quantity, cost, weight);
         }
 
         public override void PrintOrderItems()
         {
-            foreach (OrderItem item in items)
-            {
-                Console.WriteLine("{0} x {1} @ {2:C} = {3:C}", item.ProductCode, item.Quantity, item.UnitCost, item.Cost);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Total Cost of Items {0:C}", GetTotalCost());
-            Console.WriteLine();
+            order.PrintOrderItems();
         }
 
         public override double GetTotalCost()
         {
-            double total = 0;
-
-            foreach (OrderItem item in items)
-            {
-                total += item.Cost;
-            }
-            return total;
+            return order.GetTotalCost();
         }
     }
 
@@ -48,7 +36,8 @@ namespace OrdersDll
         public ExpressDeliveryOrderDecorator(PublicAbstractBase order) : base(order) {}
         public override void PrintOrderItems()
         {
-            Console.WriteLine("Shipping Costs May Apply", Environment.NewLine);
+            Console.WriteLine("Shipping Costs May Apply");
+            Console.WriteLine();
             base.PrintOrderItems();
             Console.WriteLine("Grand Total with Shipping {0:C}", GetTotalCost());
         }
@@ -56,6 +45,5 @@ namespace OrdersDll
         {
             return base.GetTotalCost() + 4.00;
         }
-
     }
 }
