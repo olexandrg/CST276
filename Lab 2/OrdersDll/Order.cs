5643bc7bf6138a9f94fd655e2c3dec336bd0bb2a
olexandrg <olexandrg@tuta.io>
2020-07-03 11:19:29 -0700

    Initial Lab 2 commit.

---
+++ Lab 2/OrdersDll/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersDll
+{
+    public class Order
+    {
+        private List<OrderItem> items = new List<OrderItem>();
+
+        public void AddItem(string productCode, int quantity, double cost, double weight)
+        {
+            items.Add(new OrderItem(productCode, quantity, cost, weight));
+        }        
+
+        public void PrintOrderItems()
+        {
+            foreach (OrderItem item in items)
+            {
+                Console.WriteLine("{0} x {1} @ {2:C} = {3:C}", item.ProductCode, item.Quantity, item.UnitCost, item.Cost);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total Cost of Items {0:C}", GetTotalCost());
+            Console.WriteLine();
+        }
+
+        public double GetTotalCost()
+        {
+            double total = 0;
+
+            foreach (OrderItem item in items)
+            {
+                total += item.Cost;
+            }
+            return total;
+        }
+    }
+}
---
+++ Lab 2/OrdersDll/OrderItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersDll
+{
+    public class OrderItem
+    {
+        public OrderItem(string productCode, int quantity, double unitCost, double unitWeight)
+        {
+            this.productCode = productCode;
+            this.quantity = quantity;
+            this.unitCost = unitCost;
+            this.unitWeight = unitWeight;
+        }
+
+        private string productCode;
+        public string ProductCode
+        {
+            get { return productCode; }
+        }
+
+        private int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+        }
+
+        private double unitCost;
+        public double UnitCost
+        {
+            get { return unitCost; }
+        }
+
+        public double Cost
+        {
+            get { return unitCost * quantity; }
+        }
+
+        private double unitWeight;
+        public double UnitWeight
+        {
+            get { return unitWeight; }
+        }
+
+        public double GetWeight
+        {
+            get { return unitWeight * quantity; }
+        }
+    }
+}
