59a949f3d7ae7d71b5afd449e9151a6182f066ec
olexandrg <olexandrg@tuta.io>
2020-08-01 13:57:23 -0700

    Working on GetInstance method.

--- Lab 6/SunriseSunsetWPF/MainVM.cs
+++ Lab 6/SunriseSunsetWPF/MainVM.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using SunriseSunsetLib;
 
 namespace SunriseSunsetWPF
@@ -30,6 +31,13 @@ namespace SunriseSunsetWPF
             this.Longitude = -122.7686344;
             this.Latitude = 45.3217219;
             this.Date = DateTime.Today;
+
+            CalculateCommand = new Relay(() =>
+            {
+                
+            });
+
+
         }
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] string propertyname = "")
@@ -37,5 +45,7 @@ namespace SunriseSunsetWPF
             PropertyChanged?.Invoke(this,
             new PropertyChangedEventArgs(propertyname));
         }
+
+        public ICommand CalculateCommand { get; set; }
     }
 }
---
+++ Lab 6/SunriseSunsetWPF/Relay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SunriseSunsetWPF
+{
+    public class Relay : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action action;
+
+        public Relay(Action action)
+        {
+            this.action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
