5bce197bffdeac4ef5c157e60b33d33c91d37cbe
olexandrg <olexandrg@tuta.io>
2020-08-06 22:13:35 -0700

    Moved file reading to appropriate class.

--- FinalExam/FinalExam/InstructorConfig.cs
+++ FinalExam/FinalExam/InstructorConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,81 +20,42 @@ namespace FinalExam
 
         public override void ProcessUserOption()
         {
-            using (System.IO.StreamReader file =
-                new System.IO.StreamReader(@"..\..\config.txt"))
+            // Open config file and store raw data
+            FileReader fileReader = new FileReader(@"..\..\config.txt");
+            fileReader.Parser();
+
+            // Store data on a member list
+            list = fileReader.GetList();
+            custom_items = fileReader.GetCustomList();
+
+            // Check Filtering
+            if (fileReader.filtering == 1)
             {
-                string line;
-                int output;
-                int start;
-                int stop;
-                int filtering;
-                int printingorder;
-
-                // 0 output to console or 1 to file
-                output = Convert.ToInt32(file.ReadLine());
-
-                // start number
-                start = Convert.ToInt32(file.ReadLine());
-
-                // stop number
-                stop = Convert.ToInt32(file.ReadLine());
-
-                // 0 for no filtering, 1 for odd, 2 for even
-                filtering = Convert.ToInt32(file.ReadLine());
-
-                // Order of printing 0 for default, 1 for reverse
-                printingorder = Convert.ToInt32(file.ReadLine());
-
-                // skip a line
-                line = file.ReadLine();
-
-                // read custom multiples and names
-                while ((line = file.ReadLine()) != "###")
-                {
-                    string[] strsplit = line.Split(',');
-                    int multiple = Convert.ToInt32(strsplit[0]);
-                    string word = strsplit[1];
-
-                    custom_items.Add(new CustomDecorator(multiple, word));
-
-                }
-
-                // read the rest of the numbers
-                while ((line = file.ReadLine()) != null)
-                {
-                    list.Add(line);
-                }
-
-
-                // Check Filtering
-                if (filtering == 1)
-                {
-                    FilterOddStrategy filterOddStrategy = new FilterOddStrategy(list);
-                    list = filterOddStrategy.GetNewList();
-                }
-                else if (filtering == 2)
-                {
-                    FilterEvenStrategy filterOddStrategy = new FilterEvenStrategy(list);
-                    list = filterOddStrategy.GetNewList();
-                }
-
-                // Check order of printing
-                if (printingorder == 1) ReverseList();
-
-                // Create a new list
-                CreateCustomListStrategy strategy = new CreateCustomListStrategy(start, stop, list, custom_items);
-                strategy.CreateList();
-                list = strategy.GetNewList();
-
-                // Check Output Type
-                if (output == 1)
-                {
-                    FileParserStrategy fileParser = new FileParserStrategy(list);
-                    fileParser.ProcessUserOption();
-                }
-                else
-                    PrintList();
+                FilterOddStrategy filterOddStrategy = new FilterOddStrategy(list);
+                list = filterOddStrategy.GetNewList();
             }
+            else if (fileReader.filtering == 2)
+            {
+                FilterEvenStrategy filterOddStrategy = new FilterEvenStrategy(list);
+                list = filterOddStrategy.GetNewList();
+            }
+
+            // Check order of printing
+            if (fileReader.printingorder == 1) ReverseList();
+
+            // Create a new list
+            CreateCustomListStrategy strategy = new CreateCustomListStrategy(fileReader.start, fileReader.stop, list, custom_items);
+            strategy.CreateList();
+            list = strategy.GetNewList();
+
+            // Check Output Type
+            if (fileReader.output == 1)
+            {
+                FileParserStrategy fileParser = new FileParserStrategy(list);
+                fileParser.ProcessUserOption();
+            }
+            else
+                PrintList();
         }
 
         public void PrintList()
---
+++ FinalExam/FizzBuzzLib/FileReader.cs
@@ -0,0 +1,80 @@
+﻿using FinalExam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLib
+{
+    public class FileReader
+    {
+        private List<string> list = new List<string>();
+        private List<CustomDecorator> custom_items = new List<CustomDecorator>();
+        string filename;
+
+        public int output { get; set; }
+        public int start { get; set; }
+        public int stop { get; set; }
+        public int filtering { get; set; }
+        public int printingorder { get; set; }
+
+        public FileReader(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public List<string> GetList()
+        {
+            return list;
+        }
+
+        public List<CustomDecorator> GetCustomList()
+        {
+            return custom_items;
+        }
+        public void Parser()
+        {
+            using (System.IO.StreamReader file =
+                new System.IO.StreamReader(filename))
+            {
+                string line;
+
+                // 0 output to console or 1 to file
+                this.output = Convert.ToInt32(file.ReadLine());
+
+                // start number
+                this.start = Convert.ToInt32(file.ReadLine());
+
+                // stop number
+                this.stop = Convert.ToInt32(file.ReadLine());
+
+                // 0 for no filtering, 1 for odd, 2 for even
+                this.filtering = Convert.ToInt32(file.ReadLine());
+
+                // Order of printing 0 for default, 1 for reverse
+                this.printingorder = Convert.ToInt32(file.ReadLine());
+
+                // skip a line
+                line = file.ReadLine();
+
+                // read custom multiples and names
+                while ((line = file.ReadLine()) != "###")
+                {
+                    string[] strsplit = line.Split(',');
+                    int multiple = Convert.ToInt32(strsplit[0]);
+                    string word = strsplit[1];
+
+                    custom_items.Add(new CustomDecorator(multiple, word));
+                }
+
+                // read the rest of the numbers
+                while ((line = file.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+        }
+    }
+}
